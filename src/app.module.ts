728a2ec1af50e9c50c78b900ba3af34e6f2bf8b2
RobertinoLirusso <robertinolirusso3@gmail.com>
2024-07-18 21:09:45 -0300

    db changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,17 @@ import { AppService } from './app.service';
 import { ClubModule } from './club/club.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true, 
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'clubs.sqlite',
+      type: process.env.DB_TYPE as 'sqlite' | 'postgres',
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
     }),
